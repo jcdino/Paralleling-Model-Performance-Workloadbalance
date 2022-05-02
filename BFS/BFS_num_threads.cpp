bb9cfd1b16f219dc340f008afb5bf41a5cae7768
Jae Yoon Chung <jordanchung96@gmail.com>
2022-05-02 10:57:06 -0400

    Add files via upload

---
+++ BFS/BFS_num_threads.cpp
@@ -0,0 +1,375 @@
+/*
+gcc -O1 -fopenmp -std=gnu11 BFS_num_threads.c -lrt -lm -o BFS_num_threads
+
+*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <time.h>
+#include "tbb/parallel_for_each.h"
+#include "tbb/task_scheduler_init.h"
+#include <iostream>
+#include <vector>
+#include "tbb/tick_count.h"
+#include "tbb/tbb_thread.h"
+#include "omp.h"
+
+// A function to generate random graph.
+using namespace tbb;
+
+double interval(struct timespec start, struct timespec end)
+{
+  struct timespec temp;
+  temp.tv_sec = end.tv_sec - start.tv_sec;
+  temp.tv_nsec = end.tv_nsec - start.tv_nsec;
+  if (temp.tv_nsec < 0) {
+    temp.tv_sec = temp.tv_sec - 1;
+    temp.tv_nsec = temp.tv_nsec + 1000000000;
+  }
+  return (((double)temp.tv_sec) + ((double)temp.tv_nsec)*1.0e-9);
+}
+
+
+
+void GenerateRandGraphs(int NOE, int NOV, int *MARIX)
+{
+	int i, j, edge[NOE][2], count;
+ 
+	i = 0;
+	// Build a connection between two random vertex.
+	while(i < NOE)
+	{
+		edge[i][0] = rand()%NOV;
+		edge[i][1] = rand()%NOV;
+
+ 
+		if(edge[i][0] == edge[i][1])
+			continue;
+		else
+		{
+			for(j = 0; j < i; j++)
+			{
+				if((edge[i][0] == edge[j][0] && edge[i][1] == edge[j][1]) || (edge[i][0] == edge[j][1] && edge[i][1] == edge[j][0]))
+				{	
+					i--;
+				}
+			}
+		}
+		i++;
+	}
+ 
+
+	for(i = 0; i < NOV; i++)
+	{
+		count = 0;
+		for(j = 0; j < NOE; j++)
+		{
+			if(edge[j][0] == i)
+			{
+				MARIX[edge[j][1]+i*NOV] = 1;
+				count++;
+			}
+			else if(edge[j][1] == i)
+			{
+				MARIX[edge[j][0]+i*NOV] = 1;
+				count++;
+			}
+			else if(j == NOE-1 && count == 0)
+				continue;
+		}
+	}
+		
+}
+
+int queue[10000000];
+void basic_BFS(int* given_graph, int NUM_NODES, int* visited){
+	//Basic implementation of BFS
+	int max_loop = 0;
+	int first = -1;
+	int visited_nodes = 0;
+	int last = 0;
+	int start_node = 3;
+	queue[last++] = start_node;
+	first++;
+	visited[start_node] = 1;
+	int cur_node;
+	int my_counter = 0;
+	while(first != last){
+		if(visited_nodes == NUM_NODES)
+			break;	
+		cur_node = queue[first++];
+		int i;
+		int curr_loop = 0;
+		for(i = 0; i < NUM_NODES; i++){
+			curr_loop++;
+			//Try with row-col original and increase performance
+			if(given_graph[cur_node*NUM_NODES + i]==1 && visited[i]==0){
+				my_counter++;
+				queue[last++] = i;
+				visited[i]=1;
+				visited_nodes++;
+			}
+		}
+		if(curr_loop > max_loop)
+			max_loop = curr_loop;
+		
+
+	}
+	printf("Max iterations is: %d\n", max_loop);
+	printf("My counter in bfs normal is: %d\n", my_counter);
+}
+
+void OMP_BFS(int* given_graph, int NUM_NODES, int* visited, int thread_num){
+	int first = -1;
+	int last = 0;
+	int visited_nodes = 0;
+	int start_node = 3;
+	queue[last++] = start_node;
+	first++;
+	visited[start_node] = 1;
+	int cur_node, k=0;
+	while(first != last){
+		if(visited_nodes == NUM_NODES)
+			break;	
+		cur_node = queue[first++];
+		int i;
+		//omp_set_num_threads(thread_num);
+		//#pragma omp parallel for shared(visited)
+		for(i = 0; i < NUM_NODES; i++){
+			//Try with row-col original and increase performance
+			if(given_graph[cur_node*NUM_NODES + i]==1 && visited[i]==0){
+				queue[last++] = i;
+				visited[i]=1;
+				visited_nodes++;
+			}
+		}
+	}
+}
+
+void OMP_BFS_static(int* given_graph, int NUM_NODES, int* visited, int thread_num){
+	int first = -1;
+	int last = 0;
+	int visited_nodes = 0;
+	int start_node = 3;
+	queue[last++] = start_node;
+	first++;
+	visited[start_node] = 1;
+	int cur_node, k=0;
+	while(first != last){
+		if(visited_nodes == NUM_NODES)
+			break;	
+		cur_node = queue[first++];
+		int i;
+		//omp_set_num_threads(thread_num);
+		//#pragma omp parallel for schedule(static) shared(visited)
+		for(i = 0; i < NUM_NODES; i++){
+			//Try with row-col original and increase performance
+			if(given_graph[cur_node*NUM_NODES + i]==1 && visited[i]==0){
+				queue[last++] = i;
+				visited[i]=1;
+				visited_nodes++;
+			}
+		}
+	}
+}
+
+void OMP_BFS_dynamic(int* given_graph, int NUM_NODES, int* visited, int thread_num){
+	int first = -1;
+	int last = 0;
+	int visited_nodes = 0;
+	int start_node = 3;
+	queue[last++] = start_node;
+	first++;
+	visited[start_node] = 1;
+	int cur_node, k=0;
+	while(first != last){
+		if(visited_nodes == NUM_NODES)
+			break;	
+		cur_node = queue[first++];
+		int i;
+		//omp_set_num_threads(thread_num);
+		//#pragma omp parallel for schedule(dynamic) shared(visited)
+		for(i = 0; i < NUM_NODES; i++){
+			//Try with row-col original and increase performance
+			if(given_graph[cur_node*NUM_NODES + i]==1 && visited[i]==0){
+				queue[last++] = i;
+				visited[i]=1;
+				visited_nodes++;
+			}
+		}
+	}
+}
+
+void OMP_BFS_guided(int* given_graph, int NUM_NODES, int* visited, int thread_num){
+	int first = -1;
+	int last = 0;
+	int visited_nodes = 0;
+	int start_node = 3;
+	queue[last++] = start_node;
+	first++;
+	visited[start_node] = 1;
+	int cur_node, k=0;
+	while(first != last){
+		if(visited_nodes == NUM_NODES)
+			break;	
+		cur_node = queue[first++];
+		int i;
+		//omp_set_num_threads(thread_num);
+		//#pragma omp parallel for schedule(guided) shared(visited)
+		for(i = 0; i < NUM_NODES; i++){
+			//Try with row-col original and increase performance
+			if(given_graph[cur_node*NUM_NODES + i]==1 && visited[i]==0){
+				queue[last++] = i;
+				visited[i]=1;
+				visited_nodes++;
+			}
+		}
+	}
+}
+
+
+
+
+
+void TBB_BFS(int* given_graph, int NUM_NODES, int* visited, int thread_num){
+	double start[8], end[8];
+	double thread_time[8];
+	task_scheduler_init tbb_init(8);
+	
+	int max_edges = 0;
+
+	int first = -1;
+	int last = 0;
+	int visited_nodes = 0;
+	int start_node = 3;
+	queue[last++] = start_node;
+	first++;
+	visited[start_node] = 1;
+	int cur_node, k=0;
+	int my_counter = 0;
+	while(first != last){
+		if(visited_nodes == NUM_NODES)
+			break;	
+		cur_node = queue[first++];
+		int i;
+		
+		//TBB Implementation
+		parallel_for(blocked_range<int>(0,NUM_NODES),
+                       [&](blocked_range<int> r)
+		    {	
+	    		int bgn = r.begin();
+	    		int nd = r.end();		
+		    	for (int i=bgn; i<nd; ++i)
+        		{
+				start[tbb::task_arena::current_thread_index()]=omp_get_wtime();
+				if(given_graph[cur_node*NUM_NODES + i]==1 && visited[i]==0){
+		    			my_counter++;	
+					queue[last++] = i;
+					visited[i]=1;
+					visited_nodes++;
+				}
+				end[tbb::task_arena::current_thread_index()]=omp_get_wtime();
+				thread_time[tbb::task_arena::current_thread_index()] += end[tbb::task_arena::current_thread_index()] - start[tbb::task_arena::current_thread_index()];
+			}
+		    });
+
+
+	}
+
+	printf("%f %f %f %f %f %f %f %f\n", thread_time[0], thread_time[1], thread_time[2], thread_time[3], thread_time[4], thread_time[5], thread_time[6], thread_time[7]);
+	printf("MY TBB COunter is %d\n", my_counter);
+}
+int main()
+{
+	int n, i, e, v;
+ 	double start, end;
+	tick_count t0, t1;
+ 	
+	v = 5000;
+	e = v*12;
+
+	for(int j=0; j<50; j++){
+
+		int* visited = (int*) malloc(v*sizeof(int));
+        	int* adjugate_matrix  = (int*) malloc(v*v*sizeof(int));
+		for(int k=0; k<v*v; k++) adjugate_matrix[k] = 0;
+	
+
+		printf("\n\nV = %d, E = %d\n", v, e);
+		printf("[Creating Graph]\n...\n");
+
+		// A function to generate a random undirected graph with e edges and v vertexes.
+		GenerateRandGraphs(e, v, adjugate_matrix);
+
+		//Initialize all nodes to unvisited
+		for(i = 0; i < v; i++) visited[i] = 0;
+
+		//printf("\n[original] ");
+		//start = omp_get_wtime(); 
+		//t0 = tick_count::now();
+		//basic_BFS(adjugate_matrix, v, visited);
+		//t1 = tick_count::now();
+		//end = omp_get_wtime(); 
+		//printf("BASIC BFS took %g seconds\n", (t1-t0).seconds());
+/*
+		for(int k=1; k<8; k++){
+			printf("\n[OPENMP- %d threads]\n", k);
+
+			//Initialize all nodes to unvisited
+			for(i = 0; i < v; i++) visited[i] = 0;
+
+			//t0 = tick_count::now();
+			//OMP_BFS(adjugate_matrix, v, visited, k);
+			//t1 = tick_count::now();
+			//end = omp_get_wtime(); 
+			//printf("Work took %g seconds\n", (t1-t0).seconds());
+		}
+*/
+		//printf("\n[OPENMP- 8 threads]\n");
+
+		//Initialize all nodes to unvisited
+		//for(i = 0; i < v; i++) visited[i] = 0;
+
+		//start = omp_get_wtime(); 
+		//t0 = tick_count::now();
+		//OMP_BFS(adjugate_matrix, v, visited, 8);
+		//t1 = tick_count::now();
+		//end = omp_get_wtime(); 
+		//printf("Work took %g seconds\n", (t1-t0).seconds());
+
+		//Initialize all nodes to unvisited
+		//for(i = 0; i < v; i++) visited[i] = 0;
+
+		//t0 = tick_count::now();
+		//OMP_BFS_static(adjugate_matrix, v, visited, 8);
+		//t1 = tick_count::now();
+		//printf("Work took %g seconds\n", (t1-t0).seconds());
+
+		//Initialize all nodes to unvisited
+		//for(i = 0; i < v; i++) visited[i] = 0;
+
+		//t0 = tick_count::now();
+		//TBB_BFS(adjugate_matrix, v, visited, 8);
+		//t1 = tick_count::now();
+		//printf("Work took %g seconds\n", (t1-t0).seconds());
+
+		//Initialize all nodes to unvisited
+		for(i = 0; i < v; i++) visited[i] = 0;
+
+		//t0 = tick_count::now();
+		TBB_BFS(adjugate_matrix, v, visited, 8);
+		//OMP_BFS_guided(adjugate_matrix, v, visited, 8);
+		//t1 = tick_count::now();
+		printf("TBB BFS took %g seconds\n", (t1-t0).seconds());
+
+		printf("\n");
+		printf("DONE!\n");
+		printf("---------------------------------------------------\n");
+
+		free(visited);
+		free(adjugate_matrix);
+		v = v+5000;
+		e = v*12;
+	}
+}
+
